21fd87bd4e5da35263c0ccfddc0e4d51c649daa8
Rama Faisal <helloramafaisal@gmail.com>
2025-06-25 06:13:24 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: "class",
+    important: "#top-sellers",
+    content: ["./resources/views/livewire/pulse/top-sellers.blade.php"],
+    corePlugins: {
+        preflight: false,
+    },
+};
